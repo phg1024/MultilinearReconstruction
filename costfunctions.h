f9b7d24409ff073432337ebfae8652ce5e0c9594
Peihong Guo <peihongguo@gmail.com>
2016-01-04 07:11:41 -0600

    Moved differential euler angle functions here.

--- costfunctions.h
+++ costfunctions.h
@@ -138,6 +138,54 @@ struct PoseCostFunction {
   CameraParameters cam_params;
 };
 
+// differential of euler angle functions
+namespace glm {
+  template<typename T>
+  GLM_FUNC_QUALIFIER tmat4x4<T, defaultp> dEulerAngleX
+    (
+      T const &angleX
+    ) {
+    T cosX = glm::cos(angleX);
+    T sinX = glm::sin(angleX);
+
+    return tmat4x4<T, defaultp>(
+      T(0), T(0), T(0), T(0),
+      T(0), -sinX, cosX, T(0),
+      T(0), -cosX, -sinX, T(0),
+      T(0), T(0), T(0), T(0));
+  }
+
+  template<typename T>
+  GLM_FUNC_QUALIFIER tmat4x4<T, defaultp> dEulerAngleY
+    (
+      T const &angleY
+    ) {
+    T cosY = glm::cos(angleY);
+    T sinY = glm::sin(angleY);
+
+    return tmat4x4<T, defaultp>(
+      -sinY, T(0), -cosY, T(0),
+      T(0), T(0), T(0), T(0),
+      cosY, T(0), -sinY, T(0),
+      T(0), T(0), T(0), T(0));
+  }
+
+  template<typename T>
+  GLM_FUNC_QUALIFIER tmat4x4<T, defaultp> dEulerAngleZ
+    (
+      T const &angleZ
+    ) {
+    T cosZ = glm::cos(angleZ);
+    T sinZ = glm::sin(angleZ);
+
+    return tmat4x4<T, defaultp>(
+      -sinZ, cosZ, T(0), T(0),
+      -cosZ, -sinZ, T(0), T(0),
+      T(0), T(0), T(0), T(0),
+      T(0), T(0), T(0), T(0));
+  }
+}
+
 struct PoseCostFunction_analytic : public ceres::SizedCostFunction<2, 3, 3> {
   PoseCostFunction_analytic(const MultilinearModel &model,
                             const Constraint2D &constraint,
