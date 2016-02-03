344215931b654ea58dfc60f31be4f37eeeab36ed
Peihong Guo <peihongguo@gmail.com>
2016-02-03 00:07:03 -0600

    Added meanshift segmentation for albedo optimization.

--- statsutils.h
+++ statsutils.h
@@ -86,6 +86,119 @@ static vector<int> FindConsistentSet(const MatrixXd& identity_weights,
 
   return consistent_set;
 }
+
+cv::Mat MeanShiftSegmentation(const cv::Mat& x, double hs, double hr, double th) {
+  cout << "Mean shift segmentation." << endl;
+  int height = x.rows, width = x.cols;
+  bool done = false;
+  int iters = 0;
+
+  // weight map for color space
+  vector<double> weight_map(255*255+1, 0);
+  for(int i=0;i<255*255+1;++i) {
+    weight_map[i] = exp(-i/(hr*hr));
+  }
+
+  cv::Mat y = x.clone();
+
+  while(!done) {
+    ++iters;
+
+    cv::Mat weightAccum(height, width, CV_64F, 0.0);
+    cv::Mat yAccum(height, width, CV_64FC3, 0.0);
+    cv::Mat xThis(height, width, CV_64FC3);
+
+    for(int i=-hs;i<=hs;++i) {
+      for(int j=-hs;j<=hs;++j) {
+        if(i==0 && j==0) continue;
+        double spatialKernel = 1.0;
+
+        #pragma omp parallel for
+        for(int r=0;r<height;++r) {
+          int r0 = r + i;
+          if(r0<0) r0 += height;
+          if(r0>=height) r0 -= height;
+
+          for(int c=0;c<width;++c) {
+            int c0 = c + j;
+            if(c0<0) c0 += width;
+            if(c0>=width) c0 -= width;
+
+            xThis.at<cv::Vec3d>(r, c) = x.at<cv::Vec3d>(r0, c0);
+          }
+        }
+
+        cv::Mat xDiffSq = y - xThis;
+
+        #pragma omp parallel for
+        for(int r=0;r<height;++r) {
+          for(int c=0;c<width;++c) {
+            cv::Vec3d pix = xDiffSq.at<cv::Vec3d>(r, c);
+            xDiffSq.at<cv::Vec3d>(r, c) = cv::Vec3d(pix[0]*pix[0] + 1, pix[1]*pix[1] + 1, pix[2]*pix[2] + 1);
+          }
+        }
+
+        cv::Mat intensityKernel(height, width, CV_64F);
+        #pragma omp parallel for
+        for(int r=0;r<height;++r) {
+          for(int c=0;c<width;++c) {
+            cv::Vec3d pix = xDiffSq.at<cv::Vec3d>(r, c);
+            intensityKernel.at<double>(r, c) = weight_map[pix[0]] * weight_map[pix[1]] * weight_map[pix[2]];
+          }
+        }
+
+        cv::Mat weightThis = intensityKernel * spatialKernel;
+
+        weightAccum += weightThis;
+        #pragma omp parallel for
+        for(int r=0;r<height;++r) {
+          for(int c=0;c<width;++c) {
+            cv::Vec3d pix = xThis.at<cv::Vec3d>(r, c);
+            yAccum.at<cv::Vec3d>(r, c) += pix * weightThis.at<double>(r, c);
+          }
+        }
+      }
+    }
+
+    cv::Mat yThis = yAccum.clone();
+    #pragma omp parallel for
+    for(int r=0;r<height;++r) {
+      for(int c=0;c<width;++c) {
+        yThis.at<cv::Vec3d>(r, c) /= (weightAccum.at<double>(r, c) + 1e-16);
+      }
+    }
+
+    double yMS = 0;
+    #pragma omp parallel for
+    for(int r=0;r<height;++r) {
+      for(int c=0;c<width;++c) {
+        cv::Vec3d p1 = yThis.at<cv::Vec3d>(r, c);
+        cv::Vec3d p0 = y.at<cv::Vec3d>(r, c);
+        yMS += fabs(round(p1[0]) - round(p0[0]));
+        yMS += fabs(round(p1[1]) - round(p0[1]));
+        yMS += fabs(round(p1[2]) - round(p0[2]));
+      }
+    }
+    yMS /= (height*width*3);
+
+    cout << "iteration " << iters << ": " << yMS << endl;
+
+    y = yThis.clone();
+    #pragma omp parallel for
+    for(int r=0;r<height;++r) {
+      for(int c=0;c<width;++c) {
+        cv::Vec3d pix = yThis.at<cv::Vec3d>(r, c);
+        y.at<cv::Vec3d>(r, c) = cv::Vec3d(round(pix[0]), round(pix[1]), round(pix[2]));
+      }
+    }
+
+    if(yMS<=th) {
+      done = true;
+    }
+  }
+
+  return y;
+}
 }
 
 #endif
