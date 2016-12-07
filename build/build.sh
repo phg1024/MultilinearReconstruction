a16443e315af048102a068e7bda3bba19d53fd4d
Peihong Guo <peihongguo@gmail.com>
2016-12-07 03:08:53 -0600

    fixed cmake files

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,9 @@
-cmake .. -DCMAKE_BUILD_TYPE=$1
+if [ "$2" = "intel" ]; then
+  echo "Using intel compilers"
+  cmake .. -DCMAKE_BUILD_TYPE=$1 -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc
+else
+  echo "Using GCC compilers"
+  cmake .. -DCMAKE_BUILD_TYPE=$1 -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
+fi
+
 make -j4
