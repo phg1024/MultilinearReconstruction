01a3cdaa865294462f06defe7de997bc3f264f0a
Peihong Guo <peihongguo@gmail.com>
2016-12-31 16:11:43 -0600

    admin

--- aam/matlab/split_channels.m
+++
@@ -1,5 +0,0 @@
-function [r, g, b] = split_channels(I)
-r = I(:,:,1);
-g = I(:,:,2);
-b = I(:,:,3);
-end
--- aam/matlab/synthesize.m
+++
@@ -1,4 +0,0 @@
-function [s, g] = synthesize(model, bs, bg, k)
-s = model.shape.P * bs + model.shape.x;
-g = model.texture{k}.P * bg + model.texture{k}.x;
-end
